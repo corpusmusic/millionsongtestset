7e6f2612b6c7f58402ee82f9f1d4955b55a590df
chuckluck44 <charley.luckhardt@hotmail.com>
2016-05-25 14:54:26 -0600

    using pandas df

--- Regression/genre_regression.py
+++ Regression/genre_regression.py
@@ -1,3 +1,7 @@
+from sklearn.linear_model import *
+import csv
+import numpy as np
+import pandas as pd
 
 # Categorical: mode, key, topic, genre
 
@@ -5,11 +9,31 @@ def read_data(filename):
     with open(filename, 'rB') as csvf:
         return [row for row in csv.reader(csvf)]
 
+def build_key_map(header):
+    key_map = {}
+    for i in range(0, len(header)):
+        key_map[header[i]] = i
+
+    return key_map
+
 def main():
-    X = read_data("MSD_regression_data.csv")
-    Y = []
-    for song in X:
-        Y.append(song[8])
-        song.pop(8)
+    df = pd.read_csv("MSD_regression_data.csv")
+    df_nc = pd.read_csv("MSD_regression_data_noncat.csv")
 
+    for cat in ['genre', 'mode', 'key', 'topic']:
+        new_df = pd.get_dummies(df[cat])
+        cols = [cat+"."+str(col) for col in new_df.columns]
+        print cols
+        new_df.columns = cols
 
+        df = df.drop(cat, axis=1)
+        df = pd.concat([df, new_df], axis=1)
+
+    print df.columns.values
+
+    lr = LogisticRegression()
+    lr.fit(df['genre'].values, df.ix[:,1:].values)
+
+    
+
+main()
