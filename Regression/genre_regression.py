984222145fa83a062b8ee2c2e1a7b49cc283a42c
chuckluck44 <charley.luckhardt@hotmail.com>
2016-05-25 15:06:50 -0600

    Fixed regression

--- Regression/genre_regression.py
+++ Regression/genre_regression.py
@@ -5,34 +5,28 @@ import pandas as pd
 
 # Categorical: mode, key, topic, genre
 
-def read_data(filename):
-    with open(filename, 'rB') as csvf:
-        return [row for row in csv.reader(csvf)]
-
-def build_key_map(header):
-    key_map = {}
-    for i in range(0, len(header)):
-        key_map[header[i]] = i
-
-    return key_map
-
-def main():
-    df = pd.read_csv("MSD_regression_data.csv")
-    df_nc = pd.read_csv("MSD_regression_data_noncat.csv")
-
-    for cat in ['genre', 'mode', 'key', 'topic']:
+def decategorize(df):
+    for cat in ['mode', 'key', 'topic']:
         new_df = pd.get_dummies(df[cat])
         cols = [cat+"."+str(col) for col in new_df.columns]
-        print cols
         new_df.columns = cols
 
         df = df.drop(cat, axis=1)
         df = pd.concat([df, new_df], axis=1)
 
-    print df.columns.values
+        return df
+  
+
+def main():
+    df = pd.read_csv("MSD_regression_data.csv")
+    df_nc = pd.read_csv("MSD_regression_data_noncat.csv")
+
+    df_decat = decategorize(df)
+
+    print df_decat.columns.values
 
     lr = LogisticRegression()
-    lr.fit(df['genre'].values, df.ix[:,1:].values)
+    lr.fit(df_decat.ix[:,1:].values, df_decat['genre'].values)
 
     
 
